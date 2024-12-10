6d88cb6749a80d18ce1731cc18a4a7a8f0fc7903
Malcomx13090 <malcomx13090@outlook.com>
2024-12-11 00:12:39 +0100

    Basic Modifs.
    Fonts
    Header
    Logo

--- app/public/wp-content/themes/mota-child/functions.php
+++ app/public/wp-content/themes/mota-child/functions.php
@@ -1,6 +1,20 @@
-<?php 
-	 add_action( 'wp_enqueue_scripts', 'mota_child_enqueue_styles' );
-	 function mota_child_enqueue_styles() {
- 		  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
- 		  } 
- ?>
+<?php
+add_action( 'wp_enqueue_scripts', 'mota_child_enqueue_styles' );
+
+function mota_child_enqueue_styles() {
+    // Enqueue the parent theme's stylesheet
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
+    // Enqueue the child theme's stylesheet (loaded after the parent style)
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style' ) );
+}
+?>
+
+<?php
+add_action( 'wp_enqueue_scripts', 'enqueue_custom_fonts' );
+
+function enqueue_custom_fonts() {
+    // Enqueue the custom font CSS file
+    wp_enqueue_style( 'custom-fonts', get_stylesheet_directory_uri() . '/fonts.css', array(), null );
+}
+?>
