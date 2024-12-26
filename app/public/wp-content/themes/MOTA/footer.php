7d69fc268b169850058d190fbaedf220d5396344
Malcomx13090 <malcomx13090@outlook.com>
2024-12-26 06:30:36 +0100

    CSS and JS
    Fixing multiple bugs fromg GH

---
+++ app/public/wp-content/themes/MOTA/footer.php
@@ -0,0 +1,14 @@
+<?php
+wp_footer();
+?>
+
+<footer>
+    <!-- Autres contenus du pied de page -->
+
+    <!-- La modale cachée par défaut -->
+    <div class="modale" id="maModale" style="display: none;">
+        <!-- Contenu de la modale -->
+        <p>Contenu de la modale</p>
+        <button id="fermerModale">Fermer</button>
+    </div>
+</footer>
--- app/public/wp-content/themes/MOTA/functions.php
+++ app/public/wp-content/themes/MOTA/functions.php
@@ -4,13 +4,29 @@ function blank_theme_enqueue_styles() {
     wp_enqueue_style( 'main-style', get_template_directory_uri() . '/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'blank_theme_enqueue_styles' );
-?>
 
-<?php
+
 add_action( 'wp_enqueue_scripts', 'enqueue_custom_fonts' );
 
 function enqueue_custom_fonts() {
     // Enqueue the custom font CSS file
     wp_enqueue_style( 'custom-fonts', get_stylesheet_directory_uri() . '/fonts.css', array(), null );
 }
-?>
+
+
+
+function register_my_menus() {
+    register_nav_menus(array(
+        'primary' => __('Menu Principal', 'votre-theme'),
+        'footer' => __('Menu Footer', 'votre-theme'),
+    ));
+}
+add_action('after_setup_theme', 'register_my_menus');
+
+
+
+
+function ajouter_mon_script() {
+    wp_enqueue_script('mon-script', get_template_directory_uri() . '/scripts/script.js', array(), null, true);
+}
+add_action('wp_enqueue_scripts', 'ajouter_mon_script');
