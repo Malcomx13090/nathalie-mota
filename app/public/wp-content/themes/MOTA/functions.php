06d75e53073f114184ae84577c6476d2e859ba26
Malcomx13090 <malcomx13090@outlook.com>
2025-01-02 11:00:43 +0100

    Taxonomies Et CPT
    Création et application

--- app/public/wp-content/themes/MOTA/functions.php
+++ app/public/wp-content/themes/MOTA/functions.php
@@ -30,3 +30,6 @@ function ajouter_mon_script() {
     wp_enqueue_script('mon-script', get_template_directory_uri() . '/scripts/script.js', array(), null, true);
 }
 add_action('wp_enqueue_scripts', 'ajouter_mon_script');
+
+
+add_theme_support('post-thumbnails');
